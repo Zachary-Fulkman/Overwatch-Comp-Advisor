f52bd9f170208fc4795d30f3ea81b6cfc66bfebd
Zachary-Fulkman <163482286+Zachary-Fulkman@users.noreply.github.com>
2025-06-05 20:11:38 -0400

    Create Map.cs
    An unfinished version of the Map Repository

---
+++ Map.cs
@@ -0,0 +1,41 @@
+namespace Overwatch_2_Suggestions
+{
+    public enum MapSelect
+    {
+        Antarctic_Peninsula,
+        Busan,
+        Ilios,
+        Lijiang_Tower,
+        Nepal,
+        Oasis,
+        Samoa,
+        Circuit_Royal,
+        Dorodo,
+        Havana,
+        Junkertown,
+        Rialto,
+        Route_66,
+        Shambali_Monastery,
+        Watchpoint_Gibraltar,
+        New_Junk_City,
+        Suravasa,
+        Blizzard_World,
+        Eichenwalde,
+        Hollywood,
+        Kings_Row,
+        Midtown,
+        Numbani,
+        Paraiso,
+        Colosseo,
+        Esperanca,
+        New_Queen_Street,
+        Runasapi,
+        Hanaoka,
+        Throne_of_Anubis
+
+    }
+    
+    public class Map
+    {
+    }
+}
