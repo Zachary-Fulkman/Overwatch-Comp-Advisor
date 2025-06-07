0df4d8aac13cf7a6427e4f84a7b62f4ef0a45cf2
Zachary-Fulkman <163482286+Zachary-Fulkman@users.noreply.github.com>
2025-06-07 11:41:50 -0400

    Create MapRepository.cs
    Added a Map Repository to hold map data

---
+++ MapRepository.cs
@@ -0,0 +1,162 @@
+namespace Overwatch_2_Suggestions
+{
+    public static class MapRepository
+    {
+        public static List<Map> GetMaps()
+        {
+            return new List<Map>
+            {
+                new Map
+                {
+                    Name = MapName.Antarctic_Peninsula,
+                    Playstyle = MapPlaystyle.Hybrid
+                },
+                new Map
+                {
+                    Name = MapName.Busan,
+                    Playstyle = MapPlaystyle.Dive
+                },
+                new Map
+                {
+                    Name = MapName.Ilios,
+                    Playstyle = MapPlaystyle.Brawl
+                },
+                new Map
+                {
+                    Name = MapName.Lijiang_Tower,
+                    Playstyle = MapPlaystyle.Dive
+                },
+                new Map
+                {
+                    Name = MapName.Nepal,
+                    Playstyle = MapPlaystyle.Brawl
+                },
+                new Map
+                {
+                    Name = MapName.Oasis,
+                    Playstyle = MapPlaystyle.Hybrid
+                },
+                new Map
+                {
+                    Name = MapName.Samoa,
+                    Playstyle = MapPlaystyle.Dive
+                },
+                new Map
+                {
+                    Name = MapName.Circuit_Royal,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Dorodo,
+                    Playstyle = MapPlaystyle.Brawl
+                },
+                new Map
+                {
+                    Name = MapName.Havana,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Junkertown,
+                    Playstyle = MapPlaystyle.Hybrid
+                },
+                new Map
+                {
+                    Name = MapName.Rialto,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Route_66,
+                    Playstyle = MapPlaystyle.Hybrid
+                },
+                new Map
+                {
+                    Name = MapName.Shambali_Monastery,
+                    Playstyle = MapPlaystyle.Hybrid
+                },
+                new Map
+                {
+                    Name = MapName.Watchpoint_Gibraltar,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.New_Junk_City,
+                    Playstyle = MapPlaystyle.Dive
+                },
+                new Map
+                {
+                    Name = MapName.Suravasa,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Blizzard_World,
+                    Playstyle = MapPlaystyle.Hybrid
+                },
+                new Map
+                {
+                    Name = MapName.Eichenwalde,
+                    Playstyle = MapPlaystyle.Brawl
+                },
+                new Map
+                {
+                    Name = MapName.Hollywood,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Kings_Row,
+                    Playstyle = MapPlaystyle.Brawl
+                },
+                new Map
+                {
+                    Name = MapName.Midtown,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Numbani,
+                    Playstyle = MapPlaystyle.Hybrid
+                },
+                new Map
+                {
+                    Name = MapName.Paraiso,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Colosseo,
+                    Playstyle = MapPlaystyle.Dive
+                },
+                new Map
+                {
+                    Name = MapName.Esperanca,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.New_Queen_Street,
+                    Playstyle = MapPlaystyle.Dive
+                },
+                new Map
+                {
+                    Name = MapName.Runasapi,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Hanaoka,
+                    Playstyle = MapPlaystyle.Poke
+                },
+                new Map
+                {
+                    Name = MapName.Throne_of_Anubis,
+                    Playstyle = MapPlaystyle.Hybrid
+                }
+            };
+        }
+    }
+}
